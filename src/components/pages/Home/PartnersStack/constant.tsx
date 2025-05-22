72c4f8bed9ec5faabaf74303bc84401738a00182
Hakeem Abbas <hakeemsyd@gmail.com>
2025-05-23 04:33:30 +0500

    svg cleanup

---
+++ src/components/pages/Home/PartnersStack/constant.tsx
@@ -0,0 +1,96 @@
+import Airbyte from '@/components/ui/icons/Airbyte';
+import Anthropic from '@/components/ui/icons/Anthropic';
+import Azures from '@/components/ui/icons/Azures';
+import Bing from '@/components/ui/icons/Bing';
+import GoogleClouds from '@/components/ui/icons/GoogleClouds';
+import GoogleDrive from '@/components/ui/icons/GoogleDrive';
+import HuggingFace from '@/components/ui/icons/HuggingFace';
+import Composio from '@/components/ui/icons/Composio';
+import Confluence from '@/components/ui/icons/Confluence';
+import Couchbase from '@/components/ui/icons/Couchbase';
+import Evernote from '@/components/ui/icons/Evernote';
+import Github from '@/components/ui/icons/Github';
+import Glean from '@/components/ui/icons/Glean';
+import Gmail from '@/components/ui/icons/Gmail';
+import Groq from '@/components/ui/icons/Groq';
+import Supa from '@/components/ui/icons/Supa';
+import LANGCHAIN from '@/components/ui/icons/LANGCHAIN';
+import SerpAI from '@/components/ui/icons/SerpAPI';
+import Amazon from '@/components/ui/icons/Amazon';
+import WolframAlpha from '@/components/ui/icons/WolframAlpha';
+import YahooFinance from '@/components/ui/icons/YahooFinance';
+import Crew from '@/components/ui/icons/Crew';
+import Meta from '@/components/ui/icons/Meta';
+import Reddis from '@/components/ui/icons/Reddis';
+import MILVUS from '@/components/ui/icons/MILVUS';
+import Mistral from '@/components/ui/icons/mistral-ai_wordmark';
+import MongoDB from '@/components/ui/icons/MongoDB';
+import Notion from '@/components/ui/icons/Notion';
+import NVIDIA from '@/components/ui/icons/NVIDIA';
+import Ollama from '@/components/ui/icons/Ollama';
+import Perplexity from '@/components/ui/icons/Perplexity';
+import Weaviate from '@/components/ui/icons/Weaviate';
+import Serper from '@/components/ui/icons/Serper';
+import Qdrant from '@/components/ui/icons/Qdrant';
+import Slack from '@/components/ui/icons/Slack';
+import Datastax from '@/components/ui/icons/Datastax';
+import Tavily from '@/components/ui/icons/Tavily';
+import Upstash from '@/components/ui/icons/Upstash';
+import Vectara from '@/components/ui/icons/Vectara';
+import Wikipedia from '@/components/ui/icons/Wikipedia';
+import Zap from '@/components/ui/icons/Zap';
+import MeriTalk from '@/components/ui/icons/MeriTalk';
+import Pinecone from '@/components/ui/icons/Pinecone';
+import Unstructured from '@/components/ui/icons/Unstructured';
+import OpenAI from '@/components/ui/icons/OpenAI';
+import Surface from '@/components/ui/icons/Surface';
+
+export const LOGOS = [
+  { name: 'Airbyte', svg: <Airbyte /> },
+  { name: 'Anthropic', svg: <Anthropic /> },
+  { name: 'Azure', svg: <Azures /> },
+  { name: 'Bing', svg: <Bing /> },
+  { name: 'Composio', svg: <Composio /> },
+  { name: 'Confluence', svg: <Confluence /> },
+  { name: 'Couchbase', svg: <Couchbase /> },
+  { name: 'Evernote', svg: <Evernote /> },
+  { name: 'Github', svg: <Github /> },
+  { name: 'Glean', svg: <Glean /> },
+  { name: 'Gmail', svg: <Gmail /> },
+  { name: 'Google Cloud', svg: <GoogleClouds /> },
+  { name: 'Google Drive', svg: <GoogleDrive /> },
+  { name: 'Groq', svg: <Groq /> },
+  { name: 'Supa', svg: <Supa /> },
+  { name: 'HuggingFace', svg: <HuggingFace /> },
+  { name: 'Langchain', svg: <LANGCHAIN /> },
+  { name: 'Meta', svg: <Meta /> },
+  { name: 'Reddis', svg: <Reddis /> },
+  { name: 'Milvus', svg: <MILVUS /> },
+  { name: 'Mistral', svg: <Mistral /> },
+  { name: 'MongoDB', svg: <MongoDB /> },
+  { name: 'Notion', svg: <Notion /> },
+  { name: 'NVIDIA', svg: <NVIDIA /> },
+  { name: 'Ollama', svg: <Ollama /> },
+  { name: 'Perplexity', svg: <Perplexity /> },
+  { name: 'Weaviate', svg: <Weaviate /> },
+  { name: 'Serper', svg: <Serper /> },
+  { name: 'Qdrant', svg: <Qdrant /> },
+  { name: 'Serp API', svg: <SerpAI /> },
+  { name: 'Slack', svg: <Slack /> },
+  { name: 'Supa', svg: <Supa /> },
+  { name: 'Datastax', svg: <Datastax /> },
+  { name: 'Amazon Bedrock', svg: <Amazon /> },
+  { name: 'Tavily', svg: <Tavily /> },
+  { name: 'Upstash', svg: <Upstash /> },
+  { name: 'Vectara', svg: <Vectara /> },
+  { name: 'Wikipedia', svg: <Wikipedia /> },
+  { name: 'Wolfram Alpha', svg: <WolframAlpha /> },
+  { name: 'Yahoo! Finance', svg: <YahooFinance /> },
+  { name: 'Zap', svg: <Zap /> },
+  { name: 'MeriTalk', svg: <MeriTalk /> },
+  { name: 'Pinecone', svg: <Pinecone /> },
+  { name: 'Cassandra', svg: <Surface /> },
+  { name: 'Unstructured', svg: <Unstructured /> },
+  { name: 'OpenAI', svg: <OpenAI /> },
+  { name: 'Crew AI', svg: <Crew /> }
+];
---
+++ src/components/ui/icons/Supa/Supa.tsx
@@ -0,0 +1,36 @@
+const Supa = () => {
+  return (
+    <svg width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g id="Group 1">
+        <path
+          id="Vector"
+          d="M10.8963 17.527C10.4457 18.0945 9.53194 17.7835 9.52108 17.0589L9.3623 6.46045H16.4887C17.7795 6.46045 18.4994 7.95131 17.6967 8.96222L10.8963 17.527Z"
+          fill="url(#paint0_linear_2002_3236)"
+        />
+        <path
+          id="Vector_2"
+          d="M10.8963 17.527C10.4457 18.0945 9.53194 17.7835 9.52108 17.0589L9.3623 6.46045H16.4887C17.7795 6.46045 18.4994 7.95131 17.6967 8.96222L10.8963 17.527Z"
+          fill="url(#paint1_linear_2002_3236)"
+          fillOpacity="0.2"
+        />
+        <path
+          id="Vector_3"
+          d="M7.9982 0.473034C8.44884 -0.094528 9.36257 0.216455 9.37343 0.941087L9.44301 11.5395H2.4058C1.11498 11.5395 0.395067 10.0487 1.19774 9.03776L7.9982 0.473034Z"
+          fill="#3ECF8E"
+        />
+      </g>
+      <defs>
+        <linearGradient id="paint0_linear_2002_3236" x1="9.3623" y1="8.8104" x2="15.696" y2="11.4667" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#249361" />
+          <stop offset="1" stopColor="#3ECF8E" />
+        </linearGradient>
+        <linearGradient id="paint1_linear_2002_3236" x1="6.55428" y1="4.96571" x2="9.44277" y2="10.4031" gradientUnits="userSpaceOnUse">
+          <stop />
+          <stop offset="1" stopOpacity="0" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export default Supa;
