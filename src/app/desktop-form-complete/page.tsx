78a825d6580d172b42bced00b9a66242132b5a2e
Tarcio <rodriguestarcio.adv@gmail.com>
2025-06-04 14:36:20 -0300

    Add metadata and SEO enhancements to layout and key pages
    Added:
    
    - title
    - description
    - keywords
    - Open Graph image preview
    - Twitter meta tags
    
    to the main layout and specific pages to improve SEO and social sharing.
    These changes aim to enhance discoverability, preview appearance, and content indexing across platforms like Google, Twitter, and Facebook.

--- src/app/desktop-form-complete/page.tsx
+++ src/app/desktop-form-complete/page.tsx
@@ -1,6 +1,14 @@
 //Components
 import Page from "@/components/layout/page";
 import Template from "@/components/pages/DesktopFormComplete/Template";
+import type { Metadata } from "next";
+
+export const generateMetadata = (): Metadata => {
+  return {
+    title: "Desktop Form Complete | Langflow",
+    description: "Langflow Desktop Form Complete",
+  };
+};
 
 const DesktopFormComplete = async () => {
   return (
