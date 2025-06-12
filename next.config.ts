0fddf23f3c58720a6380f920124682f36ff6b28b
Tejas Kumar <tejas@tejas.qa>
2025-06-12 17:24:16 +0200

    Add redirects

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  redirects: async () => {
+    return [
+      {
+        source: "/docs",
+        destination: "https://docs.langflow.org",
+        permanent: false,
+      },
+      {
+        source: "/blog",
+        destination: "https://blog.langflow.org",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
