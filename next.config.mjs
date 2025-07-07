811aea5e642484c1b4696ba4e03a14f47b1c19f2
Tejas Kumar <tejas.kumar@datastax.com>
2025-07-07 17:56:53 +0200

    Add redirects

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,31 @@ const nextConfig = {
   redirects: async () => {
     return [
       {
+        source: "/discord",
+        destination: "https://discord.com/invite/EqksyE2EX9",
+        permanent: false,
+      },
+      {
+        source: "/github",
+        destination: "https://github.com/langflow-ai/langflow",
+        permanent: false,
+      },
+      {
+        source: "/twitter",
+        destination: "https://x.com/langflow_ai",
+        permanent: false,
+      },
+      {
+        source: "/youtube",
+        destination: "https://www.youtube.com/@langflow",
+        permanent: false,
+      },
+      {
+        source: "/x",
+        destination: "https://x.com/langflow_ai",
+        permanent: false,
+      },
+      {
         source: "/docs",
         destination: "https://docs.langflow.org",
         permanent: false,
