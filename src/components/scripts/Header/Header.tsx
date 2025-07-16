9451f5fc4e31c26890ce027f569ad08615997e90
José Galdámez <eladio.galdamez@gmail.com>
2025-07-16 12:35:02 -0600

    Install Reddit pixel

--- src/components/scripts/Header/Header.tsx
+++ src/components/scripts/Header/Header.tsx
@@ -90,6 +90,14 @@ const Header = () => {
           }}();`
         }}
       />
+      {/* Reddit Pixel scripts */}
+      <Script
+        id="reddit-pixel"
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `!function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','t2_4q7ujt12');rdt('track', 'PageVisit');`,
+        }}
+      />
     </>
   );
 };
