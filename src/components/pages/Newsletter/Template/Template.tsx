a4b640b23d9e6511c2fabc94550fb95193a2c88b
Phil Nash <philnash@gmail.com>
2025-07-23 16:27:03 +1000

    Refactor Newsletter components and styles to look closer to the rest of the site
    - Updated the layout of the Desktop component for cleaner class usage.
    - Modified the Template styles to enhance background and layout responsiveness.
    - Added a button to the Template for linking to the latest issues of the newsletter.
    - Adjusted the heading styles and content alignment in the Template for better visual hierarchy.

--- src/components/pages/Newsletter/Template/Template.tsx
+++ src/components/pages/Newsletter/Template/Template.tsx
@@ -1,6 +1,8 @@
 // Components
 import { KitForm } from "../KitForm/KitForm";
 import Display from "@/components/ui/Display";
+import Button from "@/components/ui/button/Button";
+import { ButtonTypes } from "@/components/ui/button/types";
 
 // Styles
 import styles from "./styles.module.scss";
@@ -8,25 +10,37 @@ import styles from "./styles.module.scss";
 const Template = () => {
   return (
     <section className={styles.template}>
-      <div className={styles.content + " container container-wide h-100"}>
-        <div className={styles.form}>
+      <div
+        className={`${styles.content} ${styles.container} container container-wide h-100`}
+      >
+        <div className="col">
           <Display
-            className="spacer--bottom-4 text-white pt-5"
-            size={400}
+            className={`spacer--bottom-4 text-white  ${styles.heading}`}
+            size={200}
             weight={400}
+            tagName="h1"
           >
-            {"Sign up for the AI++ newsletter"}
+            {"AI++ newsletter"}
           </Display>
           <Display
             className="spacer--bottom-4 text-white pt-5"
-            size={100}
+            size={200}
             weight={400}
           >
             {
-              "Subscribe to the AI++ Newsletter to stay updated with the latest in AI, Agents and MCP."
+              "A++ is sent out every two weeks with all the latest news for developers on AI, Agents and MCP."
             }
           </Display>
 
+          <Button
+            href="https://langflow.kit.com/posts"
+            variant={ButtonTypes.BORDER}
+            className={styles.button}
+          >
+            Check out the latest issues
+          </Button>
+        </div>
+        <div className="col">
           <KitForm />
         </div>
       </div>
