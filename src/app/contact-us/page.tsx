67a1744b0a9487aa7297b0e286a477bb02b17db1
Hakeem Abbas <hakeemsyd@gmail.com>
2025-09-24 17:59:34 +0500

    fix styles

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,13 +1,15 @@
-//Components
-import Page from '@/components/layout/page';
-import Template from '@/components/pages/ContactUs/Template/Template';
-import type { Metadata } from 'next';
-import { Suspense } from 'react';
+// Dependencies
+import type { Metadata } from "next";
+import { Suspense } from "react";
+
+// Components
+import Page from "@/components/layout/page";
+import Template from "@/components/pages/ContactUs/Template/Template";
 
 export const generateMetadata = (): Metadata => {
   return {
-    title: 'Contact Us | Langflow',
-    description: 'Langflow Desktop Application'
+    title: "Contact Us | Langflow",
+    description: "Langflow Desktop Application",
   };
 };
 
--- src/components/pages/ContactUs/Template/Template.tsx
+++ src/components/pages/ContactUs/Template/Template.tsx
@@ -1,24 +1,33 @@
 // Dependencies
-import { FC, PropsWithChildren } from 'react';
+import { FC, PropsWithChildren } from "react";
 
 // Components
-import Display from '@/components/ui/Display';
-import ContactForm from '@/components/ui/ContactForm';
+import Display from "@/components/ui/Display";
+import ContactForm from "@/components/ui/ContactForm";
 
 // Styles
-import styles from './styles.module.scss';
-
+import styles from "./styles.module.scss";
 
 const Template: FC<PropsWithChildren> = () => {
   return (
     <section className={styles.template}>
       <div className={`container h-100 ${styles.container}`}>
-      <Display className={`text-white ${styles.heading}`} size={400} weight={500}>
-              We're here for you!
-      </Display>
-      <Display className={`text-white ${styles.subtitle}`} size={100} weight={400}>
-      Have a question, need premier support, or want to learn more about our professional services? We're here to help.
-      </Display>
+        <Display
+          className={`text-white ${styles.heading}`}
+          size={400}
+          weight={500}
+          tagName="h1"
+        >
+          We're here for you!
+        </Display>
+        <Display
+          className={`text-white ${styles.subtitle}`}
+          size={100}
+          weight={400}
+        >
+          Have a question, need premier support, or want to learn more about our
+          professional services? We're here to help.
+        </Display>
         <div className={`col ${styles.right}`}>
           <div className={styles.content}>
             <div className={styles.form}>
