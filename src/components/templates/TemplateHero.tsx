676008e51c9320ea1dec095fb38130aa93fb066d
Tarcio <rodriguestarcio.adv@gmail.com>
2025-10-30 12:11:21 -0300

    Share fallback icon: switch to minimal FaRegCopy (Font Awesome)\n\n- Replace link/clipboard variants with fa6 FaRegCopy for clearer copy-to-clipboard affordance\n- Keep native share SVG for supported browsers\n- Size tuned to 18px to match visual weight

--- src/components/templates/TemplateHero.tsx
+++ src/components/templates/TemplateHero.tsx
@@ -11,10 +11,10 @@ import {
   HiOutlineBookOpen,
   HiOutlineChatBubbleLeftRight,
   HiOutlineCpuChip,
-  HiOutlineLink,
   HiOutlinePhone,
   HiOutlineSparkles
 } from "react-icons/hi2";
+import { FaRegCopy } from "react-icons/fa6";
 import Toast from "../ui/Toast";
 import styles from "./TemplateHero.module.scss";
 import UseTemplateModal from "./UseTemplateModal";
@@ -232,7 +232,7 @@ export default function TemplateHero({ template, flow, className = "" }: Templat
                         <path d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92s2.92-1.31 2.92-2.92-1.31-2.92-2.92-2.92z"/>
                       </svg>
                     ) : (
-                      <HiOutlineLink size={20} />
+                      <FaRegCopy size={18} />
                     )}
                   </button>
                   <button 
